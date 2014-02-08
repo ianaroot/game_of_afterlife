afcc7609e05cab4b6f37ca4b26e48729cc0d75ac
Chermaine Spence & Phil Vargas <pair+C14jcdj.PhilVargas@devbootcamp.com>
2014-02-08 15:11:04 -0800

    humanoid and humanoidFactory, refactored and functional

--- models/humanoid.rb
+++ models/humanoid.rb
@@ -6,21 +6,21 @@ class Humanoid
   attr_accessor :type, :speed, :position
 
   def initialize(attributes = {})
-    @position = attributes[:position]
+    @position = attributes[:position] || {x: (5 + rand(791)), y: (5 + rand(791))}
     @speed = attributes[:speed]
     @type = attributes[:type]
   end
 
   def move_nearest(nearest_object)
     attracted?(nearest_object) ?
-     Pathfinder.move_towards(position, nearest_object.position, speed) :
-     Pathfinder.move_away(position, nearest_object.position, speed)
+     move_towards(position, nearest_object.position, speed) :
+     move_away(position, nearest_object.position, speed)
   end
 
   def attracted?(nearest_object)
     case type
     when :human
-      nearest_object.type == :zombie ? false : true
+      !(nearest_object.type == :zombie)
     when :zombie
       nearest_object.type == :human
     end
--- models/humanoidFactory.rb
+++ models/humanoidFactory.rb
@@ -1,28 +1,17 @@
 module HumanoidBuilder
   extend self #class << self
   def populate(humans, zombies)
-    population = []
-    population.push(create_humans(humans)).push(create_zombies(zombies))
-    population.flatten
+    create(humans, :human, 2) + create(zombies, :zombie, 1)
   end
 
   private
 
-  def create_humans(number_of_humans)
-    humans = []
-    number_of_humans.times do #random number
-      humans << Humanoid.new({type: :human, speed: 2, position:
-                              {x: 5 + rand(791), y: 5 + rand(791)}})
+  def create(number, type, speed)
+    population = []
+    number.times do
+      population << Humanoid.new(type: type, speed: speed, position:
+                              {x: 5 + rand(791), y: 5 + rand(791)})
     end
-    humans
-  end
-
-  def create_zombies(number_of_zombies)
-    zombies = []
-    number_of_zombies.times do #random number
-      zombies << Humanoid.new({type: :zombie, speed: 1,position:
-                              {x: 5 + rand(791), y: 5 + rand(791)}})
-    end
-    zombies
+    population
   end
 end
