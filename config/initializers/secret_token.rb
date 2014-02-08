76767e607c479380f40902d8a973c0899a8dd437
Apprentice <apprentice@dbc10.local>
2014-02-08 11:20:02 -0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameOfAfterlife::Application.config.secret_token = '243efad2597be5dd9f60c57aabb5e84e573c5315aac16814e0f9a333bde393057a5b12663ae92415c4d76cb5b85b5350298bfb8a7e8b2421685a5cc6b1b96735'
