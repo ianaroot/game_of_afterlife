609c9c9fd8165c156cdb3143574f13b54fcacdc2
Phil V <philipavargas@gmail.com>
2014-03-31 13:53:36 -0700

    actually add stuff to the right repo

---
+++ spec/HumanoidSpec.js
@@ -0,0 +1,26 @@
+describe("Humanoid", function(){
+  var human;
+
+  describe("A human", function(){
+
+    beforeEach(function(){
+      human = new Humanoid({'speed': 10, 'humanType': 'human'});
+    });
+
+    it("should have a speed of 10", function(){
+      expect(human.speed).toEqual(10)
+    });
+
+    it("should have a humanType of human", function(){
+      expect(human.humanType).toEqual('human')
+    });
+
+    it("should have a default lastPosition equal to position", function(){
+      expect(human.lastPosition).toEqual(human.position)
+    });
+
+    it("should have a timeSinceInfection equal to 0", function(){
+      expect(human.timeSinceInfection).toEqual(0)
+    });
+  });
+});
